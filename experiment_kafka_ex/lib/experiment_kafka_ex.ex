948ce3c1efb7c266197c450b71626cfe88391fdb
Amish Patel <amish.patel@thescore.com>
2021-01-20 14:33:07 -0500

    Add base elixir app for kafka_ex

---
+++ experiment_kafka_ex/lib/experiment_kafka_ex.ex
@@ -0,0 +1,18 @@
+defmodule ExperimentKafkaEx do
+  @moduledoc """
+  Documentation for `ExperimentKafkaEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExperimentKafkaEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ experiment_kafka_ex/test/experiment_kafka_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule ExperimentKafkaExTest do
+  use ExUnit.Case
+  doctest ExperimentKafkaEx
+
+  test "greets the world" do
+    assert ExperimentKafkaEx.hello() == :world
+  end
+end
